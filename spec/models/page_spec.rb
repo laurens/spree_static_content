38be4334319a3c4c9b842f704792b2f5057330fe
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-01-18 02:12:17 +0100

    Added tests for dealing with slugs without prefix.
    Relates to spree/spree_static_content#98

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -13,4 +13,21 @@ describe Spree::Page do
     @page.should be_valid
   end
 
+  it "should ensure that slug starts with a slash" do
+    @page.slug.should == '/test-page'
+
+    @page.slug = 'test-page-updated'
+    @page.save!
+    @page.slug.should == '/test-page-updated'
+  end
+
+  it "should #find_by_slug with or without a slash prefix" do
+    Spree::Page.find_by_slug('/test-page').should == @page
+    Spree::Page.find_by_slug('test-page').should == @page
+  end
+
+  it "should #find_by_slug! with or without a slash prefix" do
+    Spree::Page.find_by_slug!('/test-page').should == @page
+    Spree::Page.find_by_slug!('test-page').should == @page
+  end
 end
